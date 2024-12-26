f42d8508cc40b5a667f7f1b88f4b092a94a925cf
FrentzP <frentz233@gmail.com>
2024-12-26 11:45:30 -0500

    actualizar svpy

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from app.main import app  # Importa tu instancia de FastAPI desde app/main.py
 
-# Verifica que Vercel entienda que esta es una aplicación ASGI
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
