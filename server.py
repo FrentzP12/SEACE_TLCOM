14702573596175172ece00a651872a34adad6dde
FrentzP <frentz233@gmail.com>
2025-01-06 10:02:42 -0500

    upd

--- server.py
+++
@@ -1,6 +0,0 @@
-import os
-from app.main import app  # Importa tu instancia de FastAPI desde app/main.py
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
